2d07736b4f7b20501344b2bb83b34c2bb32c3cf7
Tassio Med <tassiohfmed@gmail.com>
2022-08-03 17:26:26 -0300

    :arrow_right: criando endpoints 'GET/talker' e 'GET/talker/:id'. --> REQ.1 e REQ.2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
-
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -11,6 +11,30 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+// não remover acima
+
+const TALKER_JSON = './talker.json';
+
+app.get('/talker', async (_req, res) => {
+  const stringTalker = await fs.readFile(TALKER_JSON, 'utf-8');
+  const jsonTalker = JSON.parse(stringTalker);
+  res.status(HTTP_OK_STATUS).send(jsonTalker);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const stringTalker = await fs.readFile(TALKER_JSON, 'utf-8');
+  const jsonTalker = [
+    ...JSON.parse(stringTalker),
+  ];
+  const talkerResult = jsonTalker.find((talker) => talker.id === Number(id));
+  if (!talkerResult) {
+    res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+  res.status(HTTP_OK_STATUS).json(talkerResult);
 });
 
 app.listen(PORT, () => {
