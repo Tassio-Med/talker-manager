805e46241ec371af17a8edbc8d804138f143958c
Tassio Med <tassiohfmed@gmail.com>
2022-08-04 11:57:30 -0300

    :arrow_right: Criando endpoint 'POST/login' e suas validações.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs/promises');
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -35,6 +36,27 @@ app.get('/talker/:id', async (req, res) => {
     });
   }
   res.status(HTTP_OK_STATUS).json(talkerResult);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  
+  const MANDATORY_EMAIL = { message: 'O campo "email" é obrigatório' };
+  const EMAIL_FORMAT = { message: 'O "email" deve ter o formato "email@email.com"' };
+  const MANDATORY_PASSWORD = { message: 'O campo "password" é obrigatório' };
+  const MIN_PASSWORD = { message: 'O "password" deve ter pelo menos 6 caracteres' };
+  const REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // REGEX LINK REFERENCE --> https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+  if (!email) res.status(400).json(MANDATORY_EMAIL); 
+  
+  if (!REGEX.test(email)) res.status(400).json(EMAIL_FORMAT); 
+  
+  if (!password) res.status(400).json(MANDATORY_PASSWORD); 
+
+  if (password.length < 6) res.status(400).json(MIN_PASSWORD); 
+
+  const token = crypto.randomBytes(8).toString('hex');
+  res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
